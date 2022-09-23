fa4337e493db739b1ec11b0da2b851d5c9047629
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2022-09-24 02:23:20 +1000

    Completed initial design from Amazon side of capture feature

--- books/amazon-notion-capture.py
+++ books/amazon-notion-capture.py
@@ -6,11 +6,18 @@ from bs4 import BeautifulSoup
 
 url = sys.argv[1]
 
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'lxml')
-
-print(soup.select('span[.author]'))
-
 # Cutoff the reference in the URL
 if "/ref" in url:
     url = url.split("/ref")[0]
+
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'lxml')
+
+# Get information about the book from the url
+title = soup.select_one('span#productTitle').text
+
+authors = soup.select('.contributorNameID')
+authors = ', '.join([author.text for author in authors])
+
+image = soup.select_one('img#imgBlkFront, img#ebooksImgBlkFront')['src']
+
