3c525fefb44730e4bafa2b861967f85af2704f75
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2022-09-24 14:48:27 +1000

    Completed v1 design of Amazon capture pipeline

--- books/amazon-notion-capture.py
+++ books/amazon-notion-capture.py
@@ -1,8 +1,15 @@
 import sys
+import os
 import requests
+import json
+from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 
 # Creates book entries in a Notion database from listings on Amazon
+
+load_dotenv()
+NOTION_API_KEY = os.environ['NOTION_API_KEY']
+NOTION_DATABASE_ID = os.environ['NOTION_DATABASE_ID']
 
 url = sys.argv[1]
 
@@ -18,3 +25,66 @@ title = soup.select_one('span#productTitle').text
 authors = ', '.join([author.text for author in soup.select('.contributorNameID')])
 image = soup.select_one('img#imgBlkFront, img#ebooksImgBlkFront')['src']
 
+endpoint = "https://api.notion.com/v1/pages/"
+headers = {
+    'Content-Type': 'application/json',
+    'Notion-Version': '2022-02-22',
+    'Authorization': f'Bearer {NOTION_API_KEY}'
+}
+data = json.dumps({
+    "parent": {
+        "database_id": NOTION_DATABASE_ID
+    },
+    "properties": {
+        "Cover": {
+            "type": "files",
+            "files": [
+                {
+                    "name": "Cover",
+                    "type": "external",
+                    "external": {
+                        "url": image
+                    }
+                }
+            ]
+        },
+        "Status": {
+            "type": "select",
+            "select": {
+                "name": "To Read"
+            }
+        },
+        "End": {
+            "type": "date",
+            "date": None
+        },
+        "Name": {
+            "type": "title",
+            "title": [
+                {
+                    "type": "text",
+                    "text": {
+                        "content": title
+                    }
+                }
+            ]
+        },
+        "Author": {
+            "type": "rich_text",
+            "rich_text": [
+                {
+                    "type": "text",
+                    "text": {
+                        "content": authors
+                    }
+                }
+            ]
+        },
+        "URL": {
+            "type": "url",
+            "url": url
+        }
+    }
+})
+
+print(requests.request("POST", endpoint, headers=headers, data=data))
