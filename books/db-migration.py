62d6a0be270f87cc1007389aff5db2b628c9b995
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2022-09-25 00:12:11 +1000

    Completed DB migration

--- books/db-migration.py
+++ books/db-migration.py
@@ -4,9 +4,6 @@ import pandas as pd
 import amazon_notion_capture
 
 data = pd.read_csv('database.csv')
-data = data[data['URL'].notna()]
 
 for i, r in data.iterrows():
-    amazon_notion_capture.post(r["URL"])
-
-# print(data['URL'].isna().sum())
+    print(amazon_notion_capture.post(r["URL"]))
