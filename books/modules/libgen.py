abe217b7c223c922cdaaca61bdf42734a4be5b59
Eric <73148494+ericfzhu@users.noreply.github.com>
2022-12-23 02:30:25 +1100

    Cleaned up code

--- books/modules/libgen.py
+++ books/modules/libgen.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -26,28 +28,42 @@ col_names = [
 ]
 
 
-def search_book(query, search_type="title"):
+def search_book(query: str, search_type: Literal["identifier", "title", "author"] = "title") -> list[dict]:
+    """
+    Returns a list of results that match the given filter criteria and query
+    :param query: Search query
+    :param search_type: Search by ISBN, Title, or Author of book
+    :return: List of results that match the given filter criteria and query
+    """
     if search_type not in search_queries:
         raise ValueError("Invalid search type")
 
+    # Parse the given query and fetch data from given URL
     query_parsed = "%20".join(query.split(" "))
-    search_url = f"http://gen.lib.rus.ec/search.php?req={query_parsed}&column={search_type}"
-    search_page = requests.get(search_url)
+    url = f"http://gen.lib.rus.ec/search.php?req={query_parsed}&column={search_type}"
+    response = requests.get(url)
 
-    soup = BeautifulSoup(search_page.text, "lxml")
+    soup = BeautifulSoup(response.text, "lxml")
     data = soup.find_all("table")[2]
 
+    # Extract data from the table
     raw_data = [
         [extract_td_data(td) for td in row.find_all("td")]
         for row in data.find_all("tr")[1:]
     ]
 
+    # Format raw data into a dictionary
     output_data = [dict(zip(col_names, row)) for row in raw_data]
 
     return output_data
 
 
 def extract_td_data(td):
+    """
+    Extracts data from a table cell
+    :param td: Row of an HTML table
+    :return: Data from the table cell
+    """
     if td.find("a") and td.find("a").has_attr("title") and td.find("a")["title"] != "":
         return td.a["href"]
     else:
