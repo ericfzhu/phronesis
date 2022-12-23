9a08411e0b0947feda5812de362992da00f8dd99
Eric <73148494+ericfzhu@users.noreply.github.com>
2022-12-23 15:51:10 +1100

    Updated GoodReadBook class to account for missing variables

--- books/modules/utils.py
+++ books/modules/utils.py
@@ -3,14 +3,14 @@ import json
 
 class GoodReadBook:
     def __init__(self, book: json, url: str):
-        self.name = book['name']
-        self.image = book['image']
-        self.book_format = book['bookFormat']
-        self.num_pages = book['numberOfPages']
-        self.language = book['inLanguage']
-        self.isbn = book['isbn']
-        self.rating = book['aggregateRating']['ratingValue']
-        self.rating_count = book['aggregateRating']['ratingCount']
-        self.review_count = book['aggregateRating']['reviewCount']
-        self.authors = [author['name'] for author in book['author']]
+        self.name = book.get('name')
+        self.image = book.get('image')
+        self.book_format = book.get('bookFormat')
+        self.num_pages = book.get('numberOfPages')
+        self.language = book.get('inLanguage')
+        self.isbn = book.get('isbn')
+        self.rating = book.get('aggregateRating').get('ratingValue')
+        self.rating_count = book.get('aggregateRating').get('ratingCount')
+        self.review_count = book.get('aggregateRating').get('reviewCount')
+        self.authors = [author['name'] for author in book.get('author')]
         self.url = url
