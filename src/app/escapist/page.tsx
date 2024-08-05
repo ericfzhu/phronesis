612c8adf5f6393e8fe04eb747861d57f1139366e
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2024-08-05 15:16:34 +1000

    wrong import

--- src/app/escapist/page.tsx
+++ src/app/escapist/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import StringEscaper from '@/app/escaper/StringEscaper';
+import StringEscaper from '@/app/escapist/StringEscaper';
 
 export const dynamic = 'force-static';
 
